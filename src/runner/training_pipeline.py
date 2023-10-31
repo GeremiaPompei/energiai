8d02e571bf737c83bb1002b6a74c3bcf38f3ce9a
Geremia Pompei <geremiapompei@gmail.com>
2023-10-31 09:51:26 +0100

    bugfix device

--- src/runner/training_pipeline.py
+++ src/runner/training_pipeline.py
@@ -13,13 +13,14 @@ def training_pipeline(epochs=100, batch_size=64, lr=1e-3, hidden_dim=400, latent
     device = select_device()
 
     # dataset
-    tr_dataset = SifimDataset(end=0.6)
-    vl_dataset = SifimDataset(start=0.6, end=0.8)
+    tr_dataset = SifimDataset(end=0.6).to(device)
+    vl_dataset = SifimDataset(start=0.6, end=0.8).to(device)
     tr_dataloader = torch.utils.data.DataLoader(tr_dataset, batch_size=batch_size, shuffle=shuffle)
     vl_dataloader = torch.utils.data.DataLoader(vl_dataset, batch_size=batch_size, shuffle=shuffle)
 
     # model
-    model = VAE(1, hidden_dim=hidden_dim, latent_dim=latent_dim, device=device)
+    model = VAE(tr_dataset.dataset.shape[-1], tr_dataset.dataset.shape[1], 1, hidden_dim=hidden_dim,
+                latent_dim=latent_dim, device=device)
 
     # trainer
     trainer = VAETrainer(model, tr_dataloader, vl_dataloader, device=device)
